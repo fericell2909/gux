5c2a75daa5db74e13f74fa6f79ead36d3bc303bb
ruben3185 <ruben3185@hotmail.com>
2021-08-30 16:58:48 -0500

    extra action all_pevesion

--- problem_1/views.py
+++ problem_1/views.py
@@ -3,6 +3,8 @@ from .models import Testings
 from .serializers import TestingsSerializers
 from django_filters.rest_framework import DjangoFilterBackend, filters
 from .filters import Testingsfilter
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from rest_framework.filters import SearchFilter, OrderingFilter
 
@@ -11,3 +13,12 @@ class TestingsViewSet(viewsets.ModelViewSet):
     filter_backends = [SearchFilter, DjangoFilterBackend, OrderingFilter]
     queryset = Testings.objects.all().order_by('-id')
     serializer_class = TestingsSerializers
+
+    @action(detail=False, methods=['GET'])
+    def all_pevesion(self, request):
+        queryset = Testings.objects.all()  
+        list_all_prevision = []
+        for i in  queryset:
+            list_all_prevision.append(i.fecha_prevision)
+
+        return Response ({"result": list_all_prevision})
